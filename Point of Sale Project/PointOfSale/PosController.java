e0ab0c7f5124fd00eae47bda90c0ef5bf4cba7f6
BenjaminBryan <35469178+BenjaminBryan@users.noreply.github.com>
2018-04-06 14:57:34 -0400

    Add files via upload

---
+++ Point of Sale Project/PointOfSale/PosController.java
@@ -0,0 +1,47 @@
+package PointOfSale;
+
+public class PosController {
+
+    //Here's some buttons
+
+    public void addItemToOrderClicked()
+    {
+        // code to add a selected item from table to order
+        System.out.println("Select clicked");
+
+    }
+
+    public void removeItemClicked()
+    {
+
+        // removes selected item from order
+        System.out.println("remove clicked");
+    }
+
+
+    public void endProgramClicked()
+    {
+        //code to exit program
+        System.out.println("x clicked");
+    }
+
+
+    public void confirmOrderClicked()
+    {
+        // code to generate receipt form
+        System.out.println("Confirm clicked");
+    }
+
+    public void clearOrderClicked()
+    {
+        //code to clear order
+        System.out.println("Clear clicked");
+    }
+
+
+    /*
+    Here are some things involved in receipt preview... where items go after added to purchase order and meth
+    that is involved in this
+
+     */
+}
